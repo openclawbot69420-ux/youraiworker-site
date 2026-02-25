729c4222730cffe5e5af5c5980fffa6f1bac7b56
Your AI Worker <Openclawbot69420@gmail.com>
2026-02-25 09:15:43 +0000

    Polish footer copy: remove em dash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -257,7 +257,7 @@ const Footer: React.FC = () => {
               info@youraiworker.nl
             </a>
             <span className="text-[11px] text-slate-300">
-              KvK- en btw-nummer delen we na intake
+              KvK en btw-nummer delen we na intake
             </span>
           </p>
         </div>
